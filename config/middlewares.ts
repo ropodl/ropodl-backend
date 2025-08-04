7170c254d05e5a42fab0b021bc1c50e3807c477f
ropodl <sarox14@gmail.com>
2025-08-05 02:42:19 +0545

    not backend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,29 +1,8 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          frameAncestors: ["https://ropodl.com", "self"],
-        },
-      },
-    },
-  },
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        // "http://localhost:3000",
-        // "http://localhost:1337",
-        "https://ropodl.com",
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
+  "strapi::security",
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
