e4bbe65dbc0a659f686095082d5ff48c4fe16d3e
ropodl <sarox14@gmail.com>
2025-08-05 23:02:52 +0545

    conf test

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env(
+    "PUBLIC_STRAPI_URL",
+    "https://talented-passion-a72e3db6e4.strapiapp.com"
+  ),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
