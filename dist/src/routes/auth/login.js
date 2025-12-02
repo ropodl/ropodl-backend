82cba3f6ce6652bd3d182d6151ed222a11987729
ropodl <sarox14@gmail.com>
2025-12-02 22:18:08 +0545

    removed dist

--- dist/src/routes/auth/login.js
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-const app = new Hono();
-app.get("login", (c) => {
-    return c.json({
-        test: "hi",
-    });
-});
-export default app;
--- dist/src/routes/index.js
+++
@@ -1,5 +0,0 @@
-import { Hono } from "hono";
-import login from "./auth/login.js";
-const app = new Hono();
-app.route("/auth/", login);
-export default app;
