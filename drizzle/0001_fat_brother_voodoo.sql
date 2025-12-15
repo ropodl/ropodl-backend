c4e2a80f856404e2b1c5289861b7e9dda2baeb6c
ropodl <sarox14@gmail.com>
2025-12-16 02:17:19 +0545

    chore: Delete Drizzle migration history files and snapshots.

--- drizzle/0001_fat_brother_voodoo.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "username" varchar(30) NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "fullname" varchar(120) NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "avatar" varchar;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "name";
